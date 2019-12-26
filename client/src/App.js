b3e4c0f4f4ffc66f48a68c0243445fc1d55993a3
Michelkazi <michel.m.kazi@gmail.com>
2019-12-26 12:56:14 -0500

    Added new component

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect} from 'react';
 import getRes from './getRes.js';
 import AppHeader from './components/AppHeader.js';
 import Login from './components/Login.js';
+import SignIn from './components/SignIn';
 
 function App() {
 	const [renderedRes, setRenderedRes] = useState({}) // put an empty object since res sends a json
@@ -16,7 +17,7 @@ function App() {
   return (
     <div className="App">
 			<AppHeader userName={renderedRes['fullName']} />
-			<Login />
+			<SignIn />
 		</div>
   );  
 }
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -11,17 +11,27 @@ const axios = require('axios');
 const Login = props => {
 	
 	const [email, setEmail] = useState('Controlled')
-
+	const [user, setUser] = useState({})
+	const [flash, setFlash] = useState(false)
   const handleChange = event => {
     setEmail(event.target.value);
   };
 
 	const handleSubmit = (e) => {
 		e.preventDefault()
-		axios.put('/api/v1/entrance/login', {
+		axios
+			.put('/api/v1/entrance/login', {
 			email: email,
 			password: '12345'
-		})
+			})
+			.then( res => {
+				if (res.status !== 200){
+					setFlash(true)
+				}
+				setUser(res.data)
+			}
+		)
+		console.log(user)
 		
 	} 
 
@@ -34,17 +44,19 @@ const Login = props => {
 
           <FormControl margin="normal" fullWidth>
             <InputLabel htmlFor="email">Email</InputLabel>
-            <Input id="email" onChange={handleChange} type="email" />
+            <Input id="email" name="user[email]" onChange={handleChange} type="email" />
           </FormControl>
 
           <FormControl margin="normal" fullWidth>
-            <InputLabel htmlFor="name">Name</InputLabel>
-            <Input id="name" type="text" />
+            <InputLabel htmlFor="password">Password</InputLabel>
+            <Input id="name" name="user[password]" type="password" />
           </FormControl>
 
           <FormControl margin="normal" fullWidth>
 						<Input type="submit" />
           </FormControl>
+
+					{flash && <p> WRONG </p>}
 
         </form>
       </div>	
