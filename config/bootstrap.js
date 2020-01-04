794747013b721fec79feb6953df30b5e95dff9f1
Michelkazi <michel.m.kazi@gmail.com>
2020-01-04 14:41:36 -0500

    Fake generated profiles now also have an age so I can test the profile filtering

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -17,14 +17,14 @@ module.exports.bootstrap = async function() {
 	 }
   //
 	 await User.createEach([
-		 { email: 'michel@example.com', password: await sails.helpers.passwords.hashPassword('12345'), fullName: 'Michel Kazi', dob: '1998/10/03', gender: 'male', agePreference: 20},
-		 { email: 'melanie@example.com', password: await sails.helpers.passwords.hashPassword('12345'), fullName: 'Melanie Garcia', dob: '2000/02/23', gender: 'male', agePreference: 21},
-		 { email: 'james@example.com', password: await sails.helpers.passwords.hashPassword('12345'), fullName: 'Jame Willet', dob: '1988/10/03', gender: 'male', agePreference: 30},
-		 { email: 'justin@example.com', password: await sails.helpers.passwords.hashPassword('12345'), fullName: 'Justin Mesina', dob: '1988/01/21', gender: 'male', agePreference: 40},
-		 { email: 'brian@example.com', password: await sails.helpers.passwords.hashPassword('12345'), fullName: 'Brian Peguero', dob: '1994/10/03', gender: 'male', agePreference: 18},
-		 { email: 'michael@example.com', password: await sails.helpers.passwords.hashPassword('12345'), fullName: 'Michael Law', dob: '1958/10/03', gender: 'male', agePreference: 25},
-		 { email: 'josh@example.com', password: await sails.helpers.passwords.hashPassword('12345'), fullName: 'Josh Seyda', dob: '1991/10/03', gender: 'male', agePreference: 22},
-		 { email: 'shahid@example.com', password: await sails.helpers.passwords.hashPassword('12345'), fullName: 'Shahid Sarker', dob: '1990/10/03', gender: 'male', agePreference: 30},
+		 { email: 'michel@example.com', password: await sails.helpers.passwords.hashPassword('12345'), fullName: 'Michel Kazi', dob: '1998/10/03', gender: 'male', agePreference: 20, age: 21},
+		 { email: 'melanie@example.com', password: await sails.helpers.passwords.hashPassword('12345'), fullName: 'Melanie Garcia', dob: '2000/02/23', gender: 'male', agePreference: 21, age: 20},
+		 { email: 'james@example.com', password: await sails.helpers.passwords.hashPassword('12345'), fullName: 'Jame Willet', dob: '1988/10/03', gender: 'male', agePreference: 30, age: 31},
+		 { email: 'justin@example.com', password: await sails.helpers.passwords.hashPassword('12345'), fullName: 'Justin Mesina', dob: '1988/01/21', gender: 'male', agePreference: 40, age: 31},
+		 { email: 'brian@example.com', password: await sails.helpers.passwords.hashPassword('12345'), fullName: 'Brian Peguero', dob: '1994/10/03', gender: 'male', agePreference: 18, age: 26},
+		 { email: 'michael@example.com', password: await sails.helpers.passwords.hashPassword('12345'), fullName: 'Michael Law', dob: '1958/10/03', gender: 'male', agePreference: 25, age: 62},
+		 { email: 'josh@example.com', password: await sails.helpers.passwords.hashPassword('12345'), fullName: 'Josh Seyda', dob: '1991/10/03', gender: 'male', agePreference: 22, age: 29},
+		 { email: 'shahid@example.com', password: await sails.helpers.passwords.hashPassword('12345'), fullName: 'Shahid Sarker', dob: '1990/10/03', gender: 'male', agePreference: 30, age: 30},
 	 ]);
 
 };
