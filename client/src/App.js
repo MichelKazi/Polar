0effc8a884078f82d8d30adcbbf3035d0d88496b
Michelkazi <michel.m.kazi@gmail.com>
2020-01-05 13:31:10 -0500

    Almost broke everything, but did CRA again

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,29 @@
-import React, {useContext, useState, useEffect} from 'react';
+import React from 'react'
 import ProtectedRoute from './routes/ProtectedRoute.js'
 import history from './history'
-import Home from './components/Home';
+import Home from './components/Home'
+import LogIn from './components/LogIn.js'
 import SignIn from './components/SignIn'
-import SingUp from './components/SignUp'
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import SignUp from './components/SignUp'
+import Profile from './components/Profile'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Dashboard from './components/Dashboard.js'
 
 function App() {
-	
-	useEffect( () => {
-	}, [])
-					
+
   return (
 		<BrowserRouter history = {history}>
 			<Switch>
+				<Route path="/test" exact component = {LogIn} />
 				<Route path="/" exact component = {Home} />
+				<Route path="/signup" exact component={SignUp} />
 				<Route path="/login" exact component={SignIn} />
+				<ProtectedRoute path="/profile" component={Profile} />
 				<ProtectedRoute path="/dashboard" component={Dashboard} />	
 			</Switch>
 		</BrowserRouter>
-  );  
+  )  
 }
 
-export default App;
+export default App
 
