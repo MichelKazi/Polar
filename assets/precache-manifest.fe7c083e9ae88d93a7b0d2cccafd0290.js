bee0433f0b391af2340bd26dad290d033484d3b0
Michelkazi <michel.m.kazi@gmail.com>
2020-01-10 13:06:35 -0500

    moved react build to assets

---
+++ assets/precache-manifest.fe7c083e9ae88d93a7b0d2cccafd0290.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60b3d98cf7d6b4b16bc7f3ff1c73d32f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3d7bb5a2ad613fbf305",
+    "url": "/static/js/2.6e5b0938.chunk.js"
+  },
+  {
+    "revision": "59807a34f04e25cdd365ef21d4904598",
+    "url": "/static/js/2.6e5b0938.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a02d1ba8035451e55f7d",
+    "url": "/static/js/main.51cd8735.chunk.js"
+  },
+  {
+    "revision": "ccaf5db78ae3e9b21962",
+    "url": "/static/js/runtime-main.398b6413.js"
+  },
+  {
+    "revision": "0927c5b0e62414888369e09427f41f85",
+    "url": "/static/media/logo.0927c5b0.png"
+  }
+]);
